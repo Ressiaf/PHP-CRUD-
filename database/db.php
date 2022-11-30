6ccaa92a80fd7bef9b7a83a9dcd7ef3b9aedd9ee
Ressiaf <facurd91@gmail.com>
2022-11-30 20:49:41 -0300

    first commit

---
+++ database/db.php
@@ -0,0 +1,13 @@
+<?php 
+    $conn = mysqli_connect(
+        '127.0.0.1:3307',
+        'root',
+        '' ,
+        'php_crud'
+    );
+#Si existe nuestra variable de coneccion devuelve el mensaje 
+#DB is connected *-* 
+    // if (isset($conn)){
+    //     echo 'DB is connected';
+    // }
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("database/db.php")?>
+<?php include("includes/header.php")?>
+    <h1>hello word</h1>
+<?php include("includes/footer.php")?>
+
